5e9b33d014b71bf35df8d20ba529c1a5b81390c4
arraysius <16810847+arraysius@users.noreply.github.com>
2021-12-15 18:40:18 +0800

    Add day 11

---
+++ day11_dumbo_octopus/day11_part1.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ifstream inputFile;
+
+// Directions (row, column):
+// Upper left, up, upper right
+// Left, right
+// Bottom left, down, botton right
+vector<pair<int, int>> directions = {
+    {-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1},
+};
+
+vector<vector<int>> grid(10, vector<int>(10, -1));
+vector<vector<bool>> visited;
+
+int flashes = 0;
+
+void flash(int row, int col) {
+    flashes++;
+    visited[row][col] = true;
+
+    for (pair<int, int> direction : directions) {
+        int adjRow = row + direction.first;
+        int adjCol = col + direction.second;
+
+        bool inRow = adjRow >= 0 && adjRow < 10;
+        bool inCol = adjCol >= 0 && adjCol < 10;
+        if (!(inRow && inCol) || grid[adjRow][adjCol] == 0) {
+            continue;
+        }
+
+        grid[adjRow][adjCol] = (grid[adjRow][adjCol] + 1) % 10;
+
+        if (grid[adjRow][adjCol] == 0) {
+            flash(adjRow, adjCol);
+        }
+    }
+}
+
+void puzzle() {
+    // Read input
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            char energy;
+            inputFile >> energy;
+            grid[i][j] = energy - '0';
+        }
+    }
+
+    for (int steps = 0; steps < 100; steps++) {
+        // Increment
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                grid[i][j] = (grid[i][j] + 1) % 10;
+            }
+        }
+
+        // Flashes
+        visited = vector<vector<bool>>(10, vector<bool>(10, false));
+        for (int row = 0; row < 10; row++) {
+            for (int col = 0; col < 10; col++) {
+                if (grid[row][col] == 0 && !visited[row][col]) {
+                    flash(row, col);
+                }
+            }
+        }
+    }
+
+    cout << flashes << endl;
+}
+
+int main() {
+    // inputFile.open("sample_input.txt");
+    inputFile.open("input.txt");
+
+    if (!inputFile.good()) {
+        cout << "Input file error" << endl;
+        return -1;
+    }
+
+    puzzle();
+    inputFile.close();
+
+    return 0;
+}
